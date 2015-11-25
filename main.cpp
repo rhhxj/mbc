31af3bf75e74a847e6b1776a3355774970de2260
rhhxj <20141105047M@admindeiMac-08.local>
2015-11-25 18:41:04 +0800

    排列顺序
    将五个数字按从小到大的顺序排列，运用到指针的方法

---
+++ main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  mbc
+//
+//  Created by 20141105047 on 15/11/25.
+//  Copyright (c) 2015年 20141105047. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[5],b[5],i = 0;
+    int *p =a;
+    while(i<5)
+    {
+        cin >> *p++;
+        i++;
+    }
+    for(p=&a[4],i=0;i<5;)
+    {
+        b[i]=*p;
+        cout<<b[i]<<"/t"<<endl;
+        p--;
+        i++;
+    }
+    return 0;
+}
